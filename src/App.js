ac655dada47debd71f69f7a3a452eb10a5894f50
Anass Habi <49916058+anassh00@users.noreply.github.com>
2023-02-10 02:36:56 +0100

    add private roots

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import Home from './Components/Home/Home';
 import Reservation from './Components/Reservation/Reservation';
 import Message from './Components/Message/Message';
 import Carte from './Components/Carte/Carte';
+import PrivateRoute from './Utils/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Reservation />} />
+        <Route path="/" element={<PrivateRoute><Reservation /></PrivateRoute>} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/Carte" element={<Carte />} />
-        <Route path="/Message" element={<Message />} />
+        <Route path="/Carte" element={<PrivateRoute><Carte /></PrivateRoute>} />
+        <Route path="/Message" element={<PrivateRoute><Message /></PrivateRoute>} />
         <Route path="*" exact={true} element={<Navigate replace to="/" />} />
       </Routes>
     </div>
